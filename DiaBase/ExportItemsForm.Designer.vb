b1ecd7640473b49e34e6645766aa6ccb6d4b9ea7
Rob Cassidy <robcassidy@hotmail.com.au>
2015-10-25 13:43:21 +1100

    Revert "Rev 29"
    This reverts commit 702eb245e884a2aa2061ac44ad42758fd4774629.

---
+++ DiaBase/ExportItemsForm.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ExportItemsForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ExportItemsForm))
+        Me.NewDatabaseLABEL = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SavedDatabasesLISTBOX = New System.Windows.Forms.ListBox()
+        Me.OpenDatabaseCHECKBOX = New System.Windows.Forms.CheckBox()
+        Me.DeleteItemsCHECKBOX = New System.Windows.Forms.CheckBox()
+        Me.MoveItemsExportBUTTON = New System.Windows.Forms.Button()
+        Me.MoveItemsCancelBUTTON = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label26 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.DatabaseFilenameTEXTBOX = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'NewDatabaseLABEL
+        '
+        Me.NewDatabaseLABEL.BackColor = System.Drawing.Color.Black
+        Me.NewDatabaseLABEL.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NewDatabaseLABEL.ForeColor = System.Drawing.Color.SeaShell
+        Me.NewDatabaseLABEL.Location = New System.Drawing.Point(42, 80)
+        Me.NewDatabaseLABEL.Name = "NewDatabaseLABEL"
+        Me.NewDatabaseLABEL.Size = New System.Drawing.Size(560, 82)
+        Me.NewDatabaseLABEL.TabIndex = 896
+        Me.NewDatabaseLABEL.Text = resources.GetString("NewDatabaseLABEL.Text")
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.BurlyWood
+        Me.Label7.Location = New System.Drawing.Point(45, 172)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(3, 173)
+        Me.Label7.TabIndex = 905
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.BurlyWood
+        Me.Label6.Location = New System.Drawing.Point(330, 172)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(3, 173)
+        Me.Label6.TabIndex = 904
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.BurlyWood
+        Me.Label5.Location = New System.Drawing.Point(47, 342)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(285, 3)
+        Me.Label5.TabIndex = 903
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.BurlyWood
+        Me.Label3.Location = New System.Drawing.Point(47, 172)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(285, 3)
+        Me.Label3.TabIndex = 902
+        '
+        'SavedDatabasesLISTBOX
+        '
+        Me.SavedDatabasesLISTBOX.BackColor = System.Drawing.Color.Black
+        Me.SavedDatabasesLISTBOX.ForeColor = System.Drawing.Color.White
+        Me.SavedDatabasesLISTBOX.FormattingEnabled = True
+        Me.SavedDatabasesLISTBOX.Location = New System.Drawing.Point(45, 172)
+        Me.SavedDatabasesLISTBOX.Name = "SavedDatabasesLISTBOX"
+        Me.SavedDatabasesLISTBOX.Size = New System.Drawing.Size(288, 173)
+        Me.SavedDatabasesLISTBOX.TabIndex = 901
+        '
+        'OpenDatabaseCHECKBOX
+        '
+        Me.OpenDatabaseCHECKBOX.BackColor = System.Drawing.Color.Black
+        Me.OpenDatabaseCHECKBOX.FlatAppearance.BorderColor = System.Drawing.Color.BurlyWood
+        Me.OpenDatabaseCHECKBOX.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OpenDatabaseCHECKBOX.ForeColor = System.Drawing.Color.BurlyWood
+        Me.OpenDatabaseCHECKBOX.Location = New System.Drawing.Point(350, 155)
+        Me.OpenDatabaseCHECKBOX.Name = "OpenDatabaseCHECKBOX"
+        Me.OpenDatabaseCHECKBOX.Size = New System.Drawing.Size(249, 60)
+        Me.OpenDatabaseCHECKBOX.TabIndex = 900
+        Me.OpenDatabaseCHECKBOX.Text = "Open Desination Database Once Items Are Exported"
+        Me.OpenDatabaseCHECKBOX.UseVisualStyleBackColor = False
+        '
+        'DeleteItemsCHECKBOX
+        '
+        Me.DeleteItemsCHECKBOX.BackColor = System.Drawing.Color.Black
+        Me.DeleteItemsCHECKBOX.FlatAppearance.BorderColor = System.Drawing.Color.BurlyWood
+        Me.DeleteItemsCHECKBOX.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DeleteItemsCHECKBOX.ForeColor = System.Drawing.Color.BurlyWood
+        Me.DeleteItemsCHECKBOX.Location = New System.Drawing.Point(350, 214)
+        Me.DeleteItemsCHECKBOX.Name = "DeleteItemsCHECKBOX"
+        Me.DeleteItemsCHECKBOX.Size = New System.Drawing.Size(249, 52)
+        Me.DeleteItemsCHECKBOX.TabIndex = 899
+        Me.DeleteItemsCHECKBOX.Text = "Do Not Delete Exported Items From The Source Database"
+        Me.DeleteItemsCHECKBOX.UseVisualStyleBackColor = False
+        '
+        'MoveItemsExportBUTTON
+        '
+        Me.MoveItemsExportBUTTON.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.MoveItemsExportBUTTON.FlatAppearance.BorderSize = 2
+        Me.MoveItemsExportBUTTON.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black
+        Me.MoveItemsExportBUTTON.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.MoveItemsExportBUTTON.ForeColor = System.Drawing.Color.BurlyWood
+        Me.MoveItemsExportBUTTON.Location = New System.Drawing.Point(442, 353)
+        Me.MoveItemsExportBUTTON.Margin = New System.Windows.Forms.Padding(3, 1, 3, 1)
+        Me.MoveItemsExportBUTTON.Name = "MoveItemsExportBUTTON"
+        Me.MoveItemsExportBUTTON.Size = New System.Drawing.Size(73, 25)
+        Me.MoveItemsExportBUTTON.TabIndex = 898
+        Me.MoveItemsExportBUTTON.Text = "Export"
+        Me.MoveItemsExportBUTTON.UseVisualStyleBackColor = False
+        '
+        'MoveItemsCancelBUTTON
+        '
+        Me.MoveItemsCancelBUTTON.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.MoveItemsCancelBUTTON.FlatAppearance.BorderSize = 2
+        Me.MoveItemsCancelBUTTON.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black
+        Me.MoveItemsCancelBUTTON.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.MoveItemsCancelBUTTON.ForeColor = System.Drawing.Color.BurlyWood
+        Me.MoveItemsCancelBUTTON.Location = New System.Drawing.Point(525, 353)
+        Me.MoveItemsCancelBUTTON.Margin = New System.Windows.Forms.Padding(3, 1, 3, 1)
+        Me.MoveItemsCancelBUTTON.Name = "MoveItemsCancelBUTTON"
+        Me.MoveItemsCancelBUTTON.Size = New System.Drawing.Size(73, 25)
+        Me.MoveItemsCancelBUTTON.TabIndex = 897
+        Me.MoveItemsCancelBUTTON.Text = "Cancel"
+        Me.MoveItemsCancelBUTTON.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.BurlyWood
+        Me.Label1.Location = New System.Drawing.Point(95, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(442, 23)
+        Me.Label1.TabIndex = 895
+        Me.Label1.Text = "ENTER DESTINATION DATABASE"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.BurlyWood
+        Me.Label4.Location = New System.Drawing.Point(418, 358)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(3, 20)
+        Me.Label4.TabIndex = 894
+        '
+        'Label26
+        '
+        Me.Label26.BackColor = System.Drawing.Color.BurlyWood
+        Me.Label26.Location = New System.Drawing.Point(45, 357)
+        Me.Label26.Name = "Label26"
+        Me.Label26.Size = New System.Drawing.Size(3, 20)
+        Me.Label26.TabIndex = 893
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.BurlyWood
+        Me.Label2.Location = New System.Drawing.Point(45, 375)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(376, 3)
+        Me.Label2.TabIndex = 892
+        '
+        'Label17
+        '
+        Me.Label17.BackColor = System.Drawing.Color.BurlyWood
+        Me.Label17.Location = New System.Drawing.Point(45, 356)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(376, 3)
+        Me.Label17.TabIndex = 891
+        '
+        'DatabaseFilenameTEXTBOX
+        '
+        Me.DatabaseFilenameTEXTBOX.BackColor = System.Drawing.Color.Black
+        Me.DatabaseFilenameTEXTBOX.ForeColor = System.Drawing.Color.White
+        Me.DatabaseFilenameTEXTBOX.Location = New System.Drawing.Point(46, 357)
+        Me.DatabaseFilenameTEXTBOX.Name = "DatabaseFilenameTEXTBOX"
+        Me.DatabaseFilenameTEXTBOX.Size = New System.Drawing.Size(374, 20)
+        Me.DatabaseFilenameTEXTBOX.TabIndex = 890
+        '
+        'ExportItemsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.DiaBase.My.Resources.Resources.Setting
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(641, 429)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.NewDatabaseLABEL)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.SavedDatabasesLISTBOX)
+        Me.Controls.Add(Me.OpenDatabaseCHECKBOX)
+        Me.Controls.Add(Me.DeleteItemsCHECKBOX)
+        Me.Controls.Add(Me.MoveItemsExportBUTTON)
+        Me.Controls.Add(Me.MoveItemsCancelBUTTON)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label26)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.DatabaseFilenameTEXTBOX)
+        Me.MaximizeBox = False
+        Me.Name = "ExportItemsForm"
+        Me.ShowIcon = False
+        Me.Text = "Export Items To Destination Database"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents NewDatabaseLABEL As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents SavedDatabasesLISTBOX As ListBox
+    Friend WithEvents OpenDatabaseCHECKBOX As CheckBox
+    Friend WithEvents DeleteItemsCHECKBOX As CheckBox
+    Friend WithEvents MoveItemsExportBUTTON As Button
+    Friend WithEvents MoveItemsCancelBUTTON As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label26 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents DatabaseFilenameTEXTBOX As TextBox
+End Class
