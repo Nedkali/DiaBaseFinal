eb3e67166b85fbd3200d41e8746762b61e4ebf7b
Nedkali <nedkali@westnet.com.au>
2016-01-22 17:09:22 +1100

    Rev 80
    Script file checker/replacer added

---
+++ DiaBase/ScriptChecker.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ScriptChecker
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SearchBUTTON = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.MenuText
+        Me.Label1.ForeColor = System.Drawing.Color.BurlyWood
+        Me.Label1.Location = New System.Drawing.Point(59, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.MenuText
+        Me.Label2.ForeColor = System.Drawing.Color.LawnGreen
+        Me.Label2.Location = New System.Drawing.Point(214, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.MenuText
+        Me.Label3.ForeColor = System.Drawing.Color.BurlyWood
+        Me.Label3.Location = New System.Drawing.Point(59, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.MenuText
+        Me.Label4.ForeColor = System.Drawing.Color.LawnGreen
+        Me.Label4.Location = New System.Drawing.Point(214, 69)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.MenuText
+        Me.Label5.ForeColor = System.Drawing.Color.BurlyWood
+        Me.Label5.Location = New System.Drawing.Point(59, 109)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.SystemColors.MenuText
+        Me.Label6.ForeColor = System.Drawing.Color.LawnGreen
+        Me.Label6.Location = New System.Drawing.Point(214, 109)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Label6"
+        '
+        'SearchBUTTON
+        '
+        Me.SearchBUTTON.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.SearchBUTTON.BackgroundImage = Global.DiaBase.My.Resources.Resources.ButtonBackground
+        Me.SearchBUTTON.FlatAppearance.BorderSize = 2
+        Me.SearchBUTTON.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black
+        Me.SearchBUTTON.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.SearchBUTTON.ForeColor = System.Drawing.Color.BurlyWood
+        Me.SearchBUTTON.Location = New System.Drawing.Point(115, 148)
+        Me.SearchBUTTON.Name = "SearchBUTTON"
+        Me.SearchBUTTON.Size = New System.Drawing.Size(78, 24)
+        Me.SearchBUTTON.TabIndex = 307
+        Me.SearchBUTTON.Text = "Ok"
+        Me.SearchBUTTON.UseVisualStyleBackColor = False
+        '
+        'ScriptChecker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.DiaBase.My.Resources.Resources.D2BACKGROUNDPIC
+        Me.ClientSize = New System.Drawing.Size(325, 194)
+        Me.Controls.Add(Me.SearchBUTTON)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ScriptChecker"
+        Me.Text = "ScriptChecker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents SearchBUTTON As Button
+End Class
---
+++ DiaBase/ScriptChecker.vb
@@ -0,0 +1,77 @@
+﻿Public Class ScriptChecker
+    Private Sub ScriptChecker_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim pickup As String = ""
+        Dim pickupsource As String = ""
+        Dim MuleLogger As String = ""
+        Dim MuleLoggersource As String = ""
+        Dim EntryPoint As String = AppSettings.EtalPath + "\scripts\NTDiaBaseMule.ntj"
+        Dim EntryPointsource As String = ""
+        Dim Scriptver As String = "1.0"
+
+        'Rd version file verify and copy if incorrect version
+        If AppSettings.EtalVersion = "NED" Then
+            Label1.Text = "CPickUp.ntj"
+            Label3.Text = "CBMuleLogging.ntj"
+            Label5.Text = "NTDiaBaseMule.ntj"
+            pickup = AppSettings.EtalPath + "\scripts\Common\CPickUp.ntj"
+            pickupsource = Application.StartupPath + "\Extras\CPickUp.ntj"
+            MuleLogger = AppSettings.EtalPath + "\scripts\Common\Bots\CBMuleLogging.ntj"
+            MuleLoggersource = Application.StartupPath + "\Extras\CBMuleLogging.ntj"
+            EntryPointsource = Application.StartupPath + "\Extras\RDDiaBaseMule.ntj"
+        End If
+
+        If AppSettings.EtalVersion = "PUB" Then
+            Label1.Text = "NTPickUp.ntj"
+            Label3.Text = "NTMuleLogging.ntj"
+            Label5.Text = "NTDiaBaseMule.ntj"
+            pickup = AppSettings.EtalPath + "\scripts\NTAms\NTPickUp.ntj"
+            pickupsource = Application.StartupPath + "\Extras\NTPickUp.ntj"
+            MuleLogger = AppSettings.EtalPath + "\scripts\NTBot\bots\NTMuleLogging.ntj"
+            MuleLoggersource = Application.StartupPath + "\Extras\NTMuleLogging.ntj"
+            EntryPointsource = Application.StartupPath + "\Extras\NTDiaBaseMule.ntj"
+        End If
+
+        'checks pickup script
+        If My.Computer.FileSystem.FileExists(pickup) = True Then
+            Dim ReadFile As System.IO.StreamReader = My.Computer.FileSystem.OpenTextFileReader(pickup)
+            Dim temp = ReadFile.ReadLine()
+            ReadFile.Close()
+            If temp.Contains(Scriptver) = False Then My.Computer.FileSystem.CopyFile(pickupsource, pickup, True) : Label2.Text = "File Updated"
+            If temp.Contains(Scriptver) = True Then Label2.Text = "Correct file in use"
+        End If
+        If My.Computer.FileSystem.FileExists(pickup) = False Then
+            My.Computer.FileSystem.CopyFile(pickupsource, pickup) : Label6.Text = "File Copied"
+        End If
+
+        'checks MuleLogger script
+        If My.Computer.FileSystem.FileExists(MuleLogger) = True Then
+            Dim ReadFile As System.IO.StreamReader = My.Computer.FileSystem.OpenTextFileReader(MuleLogger)
+            Dim temp = ReadFile.ReadLine()
+            ReadFile.Close()
+            If temp.Contains(Scriptver) = False Then My.Computer.FileSystem.CopyFile(MuleLoggersource, MuleLogger, True) : Label4.Text = "File Updated"
+            If temp.Contains(Scriptver) = True Then Label4.Text = "Correct file in use"
+        End If
+        If My.Computer.FileSystem.FileExists(MuleLogger) = False Then
+            My.Computer.FileSystem.CopyFile(MuleLoggersource, MuleLogger) : Label6.Text = "File Copied"
+        End If
+
+        'entry point check and replace if necessary
+        If My.Computer.FileSystem.FileExists(EntryPoint) = True Then
+            Dim ReadFile As System.IO.StreamReader = My.Computer.FileSystem.OpenTextFileReader(EntryPoint)
+            Dim temp = ReadFile.ReadLine()
+            ReadFile.Close()
+            If temp.Contains(Scriptver) = False Then My.Computer.FileSystem.CopyFile(EntryPointsource, EntryPoint, True) : Label6.Text = "File Updated"
+            If temp.Contains(Scriptver) = True Then Label6.Text = "Correct file in use"
+        End If
+
+        If My.Computer.FileSystem.FileExists(EntryPoint) = False Then
+            My.Computer.FileSystem.CopyFile(EntryPointsource, EntryPoint) : Label6.Text = "File Copied"
+        End If
+
+
+    End Sub
+
+    Private Sub SearchBUTTON_Click(sender As Object, e As EventArgs) Handles SearchBUTTON.Click
+        Me.Close()
+    End Sub
+End Class
